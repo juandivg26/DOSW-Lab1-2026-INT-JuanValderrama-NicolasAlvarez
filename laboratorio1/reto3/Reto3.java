a5d15541914485c65fc9d4e64db1018ea12e2ee6
JUANNICOLASA <juan.amunoz@mail.escuelaing.edu.co>
2026-08-08 17:45:51 -0500

    Reto#3 culminado

--- laboratorio1/reto3/Reto3.java
+++ laboratorio1/reto3/Reto3.java
@@ -1,1 +1,16 @@
-//reto3
+package reto3;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Reto3 {
+    public static void main(String[] args) {
+        String mensaje = "clave";
+
+        // Estudiante A
+        String canalA = IntStream.range(0, 3)
+                .mapToObj(i -> new StringBuilder(mensaje).toString())
+                .collect(Collectors.joining(" "));
+
+        System.out.println("Canal A: \"" + canalA + "\"");
+    }
+}
