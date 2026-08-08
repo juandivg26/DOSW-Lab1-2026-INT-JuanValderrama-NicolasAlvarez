affd693a459e57cbccbc01d37cc6716bc0fe6ee7
JUANNICOLASA <juan.amunoz@mail.escuelaing.edu.co>
2026-08-08 16:40:22 -0500

    Reto#2 culminado

--- laboratorio1/reto2/Reto2.java
+++ laboratorio1/reto2/Reto2.java
@@ -1,1 +1,14 @@
-//reto2
+package reto2;
+import java.util.*;
+
+public class Reto2 {
+    public static void main(String[] args) {
+        List<Integer> lista1 = Arrays.asList(14, 3, 27, 8, 51, 6);
+
+        // (Estudiante A): Mínimo y Cantidad
+        int min = lista1.stream().min(Integer::compare).orElse(0);
+        long cant = lista1.stream().count();
+
+        System.out.println("Min: " + min + ", Cantidad: " + cant);
+    }
+}
