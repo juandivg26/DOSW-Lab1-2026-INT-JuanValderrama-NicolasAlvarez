2e0ce05270cd460033e397f39dd640a23f7ac8e6
juandivg26 <juan.valderrama-g@mail.escuelaing.edu.co>
2026-08-08 18:21:56 -0500

    Reto#4 culminado

---
+++ laboratorio1/reto4/Reto4.java
@@ -0,0 +1,15 @@
+package reto4;
+import java.util.*;
+
+public class Reto4 {
+    public static void main(String[] args) {
+        // Estudiante B
+        Hashtable<String, Integer> mapB = new Hashtable<>();
+        mapB.put("plata", 8);
+        mapB.put("rubí", 4);
+        mapB.put("oro", 12);
+        mapB.put("esmeralda", 6);
+
+        System.out.println("Hashtable (Estudiante B): " + mapB);
+    }
+}
