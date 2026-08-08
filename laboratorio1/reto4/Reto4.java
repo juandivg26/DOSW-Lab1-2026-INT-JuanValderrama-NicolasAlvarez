9a3a03205b4017a59d894489e3fabf002e85efa3
JUANNICOLASA <juan.amunoz@mail.escuelaing.edu.co>
2026-08-08 18:20:22 -0500

    Reto#4 culminado

--- laboratorio1/reto4/Reto4.java
+++ laboratorio1/reto4/Reto4.java
@@ -1,1 +1,15 @@
+package reto4;
+import java.util.*;
 
+public class Reto4 {
+    public static void main(String[] args) {
+        // Estudiante A
+        HashMap<String, Integer> mapA = new HashMap<>();
+        mapA.putIfAbsent("oro", 5);
+        mapA.putIfAbsent("plata", 3);
+        mapA.putIfAbsent("oro", 7);
+        mapA.putIfAbsent("diamante", 10);
+
+        System.out.println("HashMap (Estudiante A): " + mapA);
+    }
+}
