371df31d3db4d8a52b09b64f7a4967821d385731
JUANNICOLASA <juan.amunoz@mail.escuelaing.edu.co>
2026-08-08 19:46:24 -0500

    refactor: Actualizar Canal A con interfaz Function y stream

--- laboratorio1/reto3/Reto3.java
+++ laboratorio1/reto3/Reto3.java
@@ -1,4 +1,6 @@
 package reto3;
+
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -6,11 +8,12 @@ public class Reto3 {
     public static void main(String[] args) {
         String mensaje = "clave";
 
-        // Estudiante A
-        String canalA = IntStream.range(0, 3)
-                .mapToObj(i -> new StringBuilder(mensaje).toString())
-                .collect(Collectors.joining(" "));
+        // Canal A
+        Function<String, String> canalA = msg ->
+                IntStream.range(0, 3)
+                        .mapToObj(i -> new StringBuilder(msg).toString())
+                        .collect(Collectors.joining(" "));
 
-        System.out.println("Canal A: \"" + canalA + "\"");
+        System.out.println("Canal A:  \"" + canalA.apply(mensaje) + "\"");
     }
 }
