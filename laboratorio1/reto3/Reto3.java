5c7ef244191d1cbb3b4d9c9a99d863d8e0ac066b
JUANNICOLASA <juan.amunoz@mail.escuelaing.edu.co>
2026-08-08 19:49:40 -0500

    fix: Solución final unificada del Reto 3 con andThen y formato oficial

--- laboratorio1/reto3/Reto3.java
+++ laboratorio1/reto3/Reto3.java
@@ -1,4 +1,6 @@
 package reto3;
+
+import java.util.Arrays;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -7,11 +9,24 @@ public class Reto3 {
     public static void main(String[] args) {
         String mensaje = "clave";
 
-        Function<String, String> descifradoFinal = msg ->
+        // Canal A
+        Function<String, String> canalA = msg ->
                 IntStream.range(0, 3)
-                        .mapToObj(i -> new StringBuffer(new StringBuilder(msg).toString()).reverse().toString())
+                        .mapToObj(i -> new StringBuilder(msg).toString())
                         .collect(Collectors.joining(" "));
 
-        System.out.println("Descifrado final: \"" + descifradoFinal.apply(mensaje) + "\"");
+        // Canal B
+        Function<String, String> canalB = msg ->
+                Arrays.stream(msg.split(" "))
+                        .map(word -> new StringBuffer(word).reverse().toString())
+                        .collect(Collectors.joining(" "));
+
+        Function<String, String> descifradoFinal = canalA.andThen(canalB);
+
+        String resultadoA = canalA.apply(mensaje);
+        String resultadoFinal = descifradoFinal.apply(mensaje);
+
+        System.out.println("Canal A:  \"" + resultadoA + "\"\n");
+        System.out.println("Descifrado final:\n\"" + resultadoFinal + "\"");
     }
 }
